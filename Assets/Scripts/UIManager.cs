5f31c97d00496b5069a4f666701527d78457f246
Lachimek12 <michal_szkola20@wp.pl>
2025-12-17 22:24:35 +0100

    final touch

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,13 +1,40 @@
+using TMPro;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    public TMP_Text PrevButtonText;
+    public GameObject NextButton;
     public GameObject[] panels;
+    public GameObject[] infoPanels;
     private int index = 0;
+    private bool isInfoVisible = false;
 
     void Start()
     {
         Show(0);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Prev(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Next();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            foreach (GameObject pan in infoPanels)
+            {
+                pan.SetActive(false);
+            }
+            isInfoVisible = false;
+        }
     }
 
     public void Show(int i)
@@ -20,6 +47,24 @@ public class UIManager : MonoBehaviour
         panels[index].SetActive(false);
         panels[i].SetActive(true);
         index = i;
+
+        if (index == 0)
+        {
+            PrevButtonText.text = "WyjdŸ";
+        }
+        else
+        {
+            PrevButtonText.text = "Wstecz";
+        }
+
+        if (index == panels.Length - 1)
+        {
+            NextButton.SetActive(false);
+        }
+        else
+        {
+            NextButton.SetActive(true);
+        }
     }
 
     public void Next()
@@ -28,9 +73,20 @@ public class UIManager : MonoBehaviour
             Show(index + 1);
     }
 
-    public void Prev()
+    public void Prev(bool isLeftArrowPress)
     {
         if (index > 0)
             Show(index - 1);
+        else if (isLeftArrowPress == false)
+            Application.Quit();
+    }
+
+    public void ToggleInfoBox()
+    { 
+        isInfoVisible = !isInfoVisible;
+        foreach (GameObject pan in infoPanels)
+        {
+            pan.SetActive(isInfoVisible);
+        }
     }
 }
